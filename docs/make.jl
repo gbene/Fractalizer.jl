c34ba6f854998501dbeb1d1b99dc66a1c166e380
gbene <gabri.benedetti@gmail.com>
2026-02-22 16:06:15 +0000

    docs again

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,7 @@ using Documenter, Fractalizer, CairoMakie
 
 pages = ["Home" => "index.md","Examples"=>"examples.md", "Types" => "types.md", "Functions" => "functions.md", "API"=>"api.md"]
 
-if "local" in ARGS
-        run(`rm -rf local_build/`)
-        makedocs(sitename="Fractalizer.jl", pages=pages, format = Documenter.HTML(
-        prettyurls = false), build="local_build", clean=true)
-else
-        makedocs(sitename="Fractalizer.jl", pages=pages, clean=true)
-end
+makedocs(sitename="Fractalizer.jl", pages=pages, clean=true)
+
 
 deploydocs(repo="github.com:gbene/Fractalizer.jl.git")
