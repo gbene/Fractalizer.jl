171c9ff64ceb1b558e85ff94ec102d478eaf22f4
gbene <gabri.benedetti@gmail.com>
2026-02-24 10:23:57 +0000

    More control over random using Vector of seeds. [docs]

--- src/random.jl
+++ src/random.jl
@@ -11,16 +11,27 @@ Type used to encapsulate the random settings for generating random templates.
 - `frequency_range::AbstractRange` -- Range of frequency to be used
 - `phase_range::AbstractRange` -- Range of phases to be used
 - `resolution::Int` -- N of points of the generated random signal
-- `iterations::Int` -- N of times that random signals are stacked 
+- `iterations::Int` -- N of times that random signals are stacked
 - `nsamples::Int` -- N of samples of the final random template
-- `seed::Int` -- seed for random generation
+- `seeds::Vector` -- 4 element vector of seeds for random generation [ampl_seed, freq_seed, phas_seed, indx_seed]. Use `nothing` for random seed. Default is [nothing, nothing, nothing, nothing]
 
 
 ### Examples
 
 - `noise_params = NoiseParams(0.1:0.1, 1.0:1:10.0, -10.0:1:10.0, 100, 4, 10)` -- default constructor, random seed
 
-- `noise_params = NoiseParams(0.1:0.1, 1.0:1:10.0, -10.0:1:10.0, 100, 4, 10, seed=1)` -- default constructor, fixed seed
+- `noise_params = NoiseParams(0.1:0.1, 1.0:1:10.0, -10.0:1:10.0, 100, 4, 10, seed=[1, nothing, 1, nothing])` -- default constructor, fixed seed for amplitude and phase
+
+
+### Notes
+
+Seeds:
+
+- `ampl_seed` controls the random pick for the amplitude range
+- `freq_seed` controls the random pick for the frequency range
+- `phas_seed` controls the random pick for the phase range
+- `indx_seed` controls which indexes of the samples are taken from the original random signal. The number of samples is controlled by nsamples.
+
 """
 struct NoiseParams
     amplitude_range::AbstractRange
@@ -30,13 +41,13 @@ struct NoiseParams
     resolution::Int
     iterations::Int
     nsamples::Int
-    seed::Int
+    seeds::Vector
 
-    function NoiseParams(amplitude_range, frequency_range, phase_range, resolution, iterations, nsamples; seed::Int=0)
+    function NoiseParams(amplitude_range, frequency_range, phase_range, resolution, iterations, nsamples; seeds=[nothing, nothing, nothing, nothing])
         if nsamples <= 0
             nsamples = resolution
         end
-        new(amplitude_range, frequency_range, phase_range, resolution, iterations, nsamples, seed)
+        new(amplitude_range, frequency_range, phase_range, resolution, iterations, nsamples, seeds)
     end
 
 
@@ -51,7 +62,8 @@ Generate a random template given the parameters in NoiseParams
 
 ### Input
 
-- `params` -- NoiseParams struct with all the settings for random generation 
+- `params` -- NoiseParams struct with all the settings for random generation
+- `prng`   -- Random generator to use for picking values. Default is Xoshiro
 
 ### Output
 
@@ -63,31 +75,28 @@ The random template is defined as follows:
 
 1. A random value of amplitude, frequency and phase are picked from the ranges
 2. A sine wave in (0,2π) is generated with the picked resolution and the random values
-3. A random selection of nsamples are then picked from the signal 
+3. A random selection of nsamples are then picked from the signal
 
 """
-function random_template(params::NoiseParams)
-
-    if params.seed != 0
-        Random.seed!(params.seed)
-    end
+function random_template(params::NoiseParams; prng::DataType=Xoshiro)
 
     amplitude_range = params.amplitude_range
     frequency_range = params.frequency_range
     phase_range = params.frequency_range
     resolution = params.resolution
     iter = params.iterations
+    ampl_seed, freq_seed, phas_seed, index_seed = params.seeds
 
     nsamples = params.nsamples
 
-    rand_index = sort(rand(1:resolution,nsamples))
     xp = range(0, 2π, resolution)
     yp = zeros(length(xp))
+    rand_index = sort(rand(prng(index_seed), 1:resolution, nsamples))
 
     for i in 1:iter
-        rand_a = rand(amplitude_range)
-        rand_f = rand(frequency_range)
-        rand_p = rand(phase_range)
+        rand_a = rand(prng(ampl_seed), amplitude_range)
+        rand_f = rand(prng(freq_seed), frequency_range)
+        rand_p = rand(prng(phas_seed), phase_range)
         @. yp += rand_a*sin(rand_f*xp+rand_p)
     end
     template = Template([xp[rand_index] yp[rand_index]])
